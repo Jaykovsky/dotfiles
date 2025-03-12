7ce0ec89347c247518b9c674c1748b47b8935264
jaykovsky <jaissonandres21@gmail.com>
2025-03-12 18:33:07 -0500

    --new neovim and rofi config files

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,15 +1,21 @@
-require('craftzdog.base')
-require('craftzdog.highlights')
-require('craftzdog.maps')
-require('craftzdog.plugins')
+require "core"
 
-local has = vim.fn.has
-local is_mac = has "macunix"
-local is_win = has "win32"
+local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", false)[1]
 
-if is_mac then
-  require('craftzdog.macos')
+if custom_init_path then
+  dofile(custom_init_path)
 end
-if is_win then
-  require('craftzdog.windows')
+
+require("core.utils").load_mappings()
+
+local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+
+-- bootstrap lazy.nvim!
+if not vim.loop.fs_stat(lazypath) then
+  require("core.bootstrap").gen_chadrc_template()
+  require("core.bootstrap").lazy(lazypath)
 end
+
+dofile(vim.g.base46_cache .. "defaults")
+vim.opt.rtp:prepend(lazypath)
+require "plugins"
